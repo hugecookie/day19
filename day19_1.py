d5a68d10d30a4c17eb359d8f05cf99f42a7bd994
hwarang <phrpp5770@gmail.com>
2023-02-09 22:16:14 +0900

    day19_10.2

--- day19_1.py
+++ day19_1.py
@@ -1,18 +1,24 @@
-def notation(n, base):
-    if n < base:
-        print(numberChar[n], end=' ')
+from tkinter import *
+
+
+def drawTriangle(x, y, size):
+    if size >= 30:
+        drawTriangle(x, y, size / 2)
+        drawTriangle(x + size / 2, y, size / 2)
+        drawTriangle(x + size / 4, int(y - size * (3 ** 0.5) / 4), size / 2)
     else:
-        notation(base, n // base)
-        print(numberChar[n % base], end=' ')
+        canvas.create_polygon(x, y, x + size, y, x + size / 2, y - size * (3 ** 0.5) / 2, fill='red', outline="red")
 
 
-numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-numberChar += ['A', 'B', 'C', 'D', 'E', 'F']
+wSize = 1000
+radius = 400
 
-number = int(input('input number -->'))
-print('\n binary : ', end=' ')
-notation(number, 2)
-print('\n octal : ', end=' ')
-notation(number, 8)
-print('\n hexadecimal : ', end=' ')
-notation(number, 16)
+
+window = Tk()
+window.title("triangle fractal")
+canvas = Canvas(window, height=wSize, width=wSize, bg='white')
+
+drawTriangle(wSize / 5, wSize / 5 * 4, wSize * 2 / 3)
+
+canvas.pack()
+window.mainloop()
