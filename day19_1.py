0aa7fd8197ac0bb90173f18644be9f7ff729f6e0
hwarang <phrpp5770@gmail.com>
2023-02-09 22:27:28 +0900

    day19_11.2

--- day19_1.py
+++ day19_1.py
@@ -1,18 +1,28 @@
-def score_sort(ary):
+def selection_sort(ary):
     n = len(ary)
-    for end in range(1, n):
-        for cur in range(end, 0, -1):
-            if ary[cur - 1][1] > ary[cur][1]:
-                ary[cur - 1], ary[cur] = ary[cur], ary[cur - 1]
+    for i in range(0, n - 1):
+        min_idx = i
+        for k in range(i + 1, n):
+            if ary[min_idx] > ary[k]:
+                min_idx = k
+        tmp = ary[i]
+        ary[i] = ary[min_idx]
+        ary[min_idx] = tmp
+
     return ary
 
 
-scoreAry = [['A', 88], ['B', 99], ['C', 71], ['D', 78], ['E', 67], ['F', 92]]
+ary2 = [[55, 33, 250, 44],
+        [88, 1, 67, 23],
+        [199, 222, 38, 47],
+        [155, 145, 20, 99]]
+ary1 = []
 
-print('before sort -->', scoreAry)
-scoreAry = score_sort(scoreAry)
-print('after sort -->', scoreAry)
+for i in range(len(ary2)):
+    for k in range(len(ary2[i])):
+        ary1.append(ary2[i][k])
 
-print('## Score ##')
-for i in range(len(scoreAry) // 2):
-    print(scoreAry[i][0], ':', scoreAry[len(scoreAry) - 1 - i][0])
+print('before sort -->', ary1)
+ary1 = selection_sort(ary1)
+print('after sort -->', ary1)
+print('median --> ', ary1[len(ary1) // 2])
